ff85b8226cb6fd2f67d4399d4e16db303ed79a3f
Jan Kanis <jan.code@jankanis.nl>
2016-07-19 16:01:17 +0200

    Remove wrong copyright attributions

--- R/getPatientSetID.R
+++ R/getPatientSetID.R
@@ -1,5 +1,4 @@
 # Copyright 2014 - 2016 The Hyve B.V.
-# Copyright 2014 Janssen Research & Development, LLC.
 #
 # This file is part of tranSMART R Client: R package allowing access to
 # tranSMART's data via its RESTful API.
